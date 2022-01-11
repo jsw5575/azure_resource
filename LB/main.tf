d8b0309f0cc0dab44b81641263dfc35188e4b0e4
jsw5575 <jsw4540@naver.com>
2022-01-11 15:34:06 +0900

    change resource v3

---
+++ LB/main.tf
@@ -0,0 +1,134 @@
+resource "azurerm_resource_group" "example" {
+  name     = "LoadBalancerRG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIPForLB"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+## Public Front End IP ###
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+
+
+### Back end pool 생성 ###
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "BackEndAddressPool"
+}
+
+/* 
+#### Backend Pool 옵션 ###
+
+data "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = "example-resources"
+}
+
+data "azurerm_lb" "example" {
+  name                = "example-lb"
+  resource_group_name = "example-resources"
+}
+
+data "azurerm_lb_backend_address_pool" "example" {
+  name            = "first"
+  loadbalancer_id = data.azurerm_lb.example.id
+}
+
+resource "azurerm_lb_backend_address_pool_address" "example" {
+  name                    = "example"
+  backend_address_pool_id = data.azurerm_lb_backend_address_pool.example.id
+  virtual_network_id      = data.azurerm_virtual_network.example.id
+  ip_address              = "10.0.0.1"
+}
+*/
+
+/*
+#### Helath Probe ####
+resource "azurerm_resource_group" "example" {
+  name     = "LoadBalancerRG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIPForLB"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_probe" "example" {
+  resource_group_name = azurerm_resource_group.example.name
+  loadbalancer_id     = azurerm_lb.example.id
+  name                = "ssh-running-probe"
+  port                = 22
+}
+
+*/
+
+/*
+#### Azure RULE ###
+
+resource "azurerm_resource_group" "example" {
+  name     = "LoadBalancerRG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIPForLB"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_rule" "example" {
+  resource_group_name            = azurerm_resource_group.example.name
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+*/
+
+
+
+
---
+++ Linux_managed_disk_Attached/variable.tf
@@ -0,0 +1,6 @@
+variable "prefix" {
+description = "common resource name rule "
+type = string
+default = "swjeong"
+}
+
